d14a6f2183e2e02a405bdeb41e16243d4c74e321
noahd <noah.decroix3@gmail.com>
2022-04-21 17:45:25 +0200

    edit home page style

--- View/home/home.html.php
+++ View/home/home.html.php
@@ -1,23 +1,29 @@
+<div id="menu-container">
+    <div class="menu">
+        <a href="?c=home&a=game">
+            <div class="home-menu"><i class="fas fa-gamepad"></i></div>
+        </a>
 
-<div class="menu">
-    <a href="?c=home&a=game">
-        <div class="home-menu"><i class="fas fa-gamepad"></i></div>
-    </a>
+        <a href="">
+            <div class="home-menu"><i class="fas fa-comments"></i></div>
+        </a>
 
-    <a href="">
-        <div class="home-menu"><i class="fas fa-comments"></i></div>
-    </a>
+        <a href="">
+            <div class="home-menu"><i class="far fa-newspaper"></i></div>
+        </a>
 
-    <a href="">
-        <div class="home-menu"><i class="far fa-newspaper"></i></div>
-    </a>
+        <a href="/?c=user&a=contact">
+            <div class="home-menu"><i class="fas fa-envelope"></i></div>
+        </a>
 
-    <a href="/?c=user&a=contact">
-        <div class="home-menu"><i class="fas fa-envelope"></i></div>
-    </a>
+        <a href="">
+            <div class="home-menu"><i class="fab fa-discord"></i></div>
+        </a>
 
-    <a href="">
-        <div class="home-menu"><i class="fab fa-discord"></i></div>
-    </a>
-
+    </div>
 </div>
+
+<footer id="home-footer">
+    <div>footer</div>
+</footer>
+
