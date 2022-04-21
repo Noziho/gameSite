140ab4fdf4751a613075aea9861c260126df21c1
noahd <noah.decroix3@gmail.com>
2022-04-21 11:23:58 +0200

    start home menu

--- View/home/home.html.php
+++ View/home/home.html.php
@@ -1,1 +1,23 @@
-<h1>HOME SWEET HOME</h1>
+
+<div class="menu">
+    <a href="?c=home&a=game">
+        <div class="home-menu"><i class="fas fa-gamepad"></i></div>
+    </a>
+
+    <a href="">
+        <div class="home-menu"><i class="fas fa-comments"></i></div>
+    </a>
+
+    <a href="">
+        <div class="home-menu"><i class="far fa-newspaper"></i></div>
+    </a>
+
+    <a href="">
+        <div class="home-menu"><i class="fas fa-envelope"></i></div>
+    </a>
+
+    <a href="">
+        <div class="home-menu"><i class="fab fa-discord"></i></div>
+    </a>
+
+</div>
