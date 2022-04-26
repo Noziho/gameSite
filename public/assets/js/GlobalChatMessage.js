be2a207d5d53b5569c827f0c5e9067138815d8cd
noahd <noah.decroix3@gmail.com>
2022-04-26 21:43:55 +0200

    fix come error for chat

--- public/assets/js/GlobalChatMessage.js
+++ public/assets/js/GlobalChatMessage.js
@@ -11,29 +11,25 @@ if (message) {
                 })
             })
                 .then(response => response.json())
-                .then(response => {
-                    message.value = '';
-                });
 
-            setInterval( function () {
-                fetch('/?c=globalChat&a=get-all', {method: 'POST'})
-                    .then(response => response.json())
-                    .then(response => {
-                        refreshChat(response);
-                        console.log(response);
-                    });
-            }, 1000);
+            message.value = '';
         }
     })
+    setInterval(function () {
+        fetch('/?c=globalChat&a=get-all', {method: 'POST'})
+            .then(response => response.json())
+            .then(response => {
+                refreshChat(response);
+                console.log(response);
+            });
+    }, 1000);
 
+    function refreshChat(messages) {
+        containerMessage.innerHTML = ''
+        for (let i = 0; i < messages.length; i++) {
+            containerMessage.innerHTML += "<div class='message'>" + "<p class='user'>" +
+                messages[i]['author'] + "</p>" + "<p>" + messages[i]['content'] + "</p>" + "</div>";
 
-}
-
-function refreshChat (messages) {
-    containerMessage.innerHTML = ''
-    for (let i = 0; i < messages.length; i++) {
-        containerMessage.innerHTML += "<div class='message'>" + "<p class='user'>" +
-            messages[i]['author'] + "</p>" + "<p>" + messages[i]['content'] + "</p>" + "</div>";
-
+        }
     }
 }
