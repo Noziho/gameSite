8f57c8e660263ca41b8ad796ffe7fab11abd2ca3
noahd <noah.decroix3@gmail.com>
2022-05-11 15:30:52 +0200

    increase lenght of text message

--- View/chat/forza.html.php
+++ View/chat/forza.html.php
@@ -17,7 +17,7 @@ use App\Controller\AbstractController;
         elseif (!AbstractController::isMuted()) { ?>
             <div>
                 <input class="send-message" type="text" name="message"
-                       placeholder="Écrivez votre message, appuyez sur entrer pour l'envoyer !" minlength="1" maxlength="300" required>
+                       placeholder="Écrivez votre message, appuyez sur entrer pour l'envoyer !" minlength="1" maxlength="1000" required>
             </div><?php
         } else { ?>
             <p class="send-message">Vous êtes mute</p><?php
