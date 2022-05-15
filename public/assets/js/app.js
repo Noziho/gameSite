e29134d621bf969d4e2b90da17dd46fed46746ed
noahd <noah.decroix3@gmail.com>
2022-05-15 12:49:11 +0200

    add checkpassword with JS reggex

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,35 +8,54 @@ const menu = $(".header_menu");
 const email = document.querySelector('#email');
 
 
- function checkRange (min, max , input, errorMessage) {
-    input.addEventListener('input', function (event) {
-        if (this.value.length > min && this.value.length < max) {
-            this.setCustomValidity("");
-            this.style.outline = "1px solid green"
+ function checkRange(min, max , input, errorMessage) {
+    input.addEventListener("input", function () {
+        if (input.value.length > min && input.value.length < max) {
+            input.setCustomValidity("");
+            input.style.outline = "1px solid green"
 ;        }
         else {
-            this.setCustomValidity(errorMessage);
-            this.style.outline = "1px solid red";
+            input.setCustomValidity(errorMessage);
+            input.style.outline = "1px solid red";
         }
     })
 
 }
 
-function checkMail (mail) {
-    return mail.match(/[a-z0-9]+@[a-z]+.[a-z]{2,3}/)
+function checkMail(mail) {
+    return mail.match(/[a-z\d]+@[a-z]+.[a-z]{2,3}/)
 }
 
-function mailValidity (input) {
+function mailValidity(input) {
+     if (input) {
+         input.addEventListener("input", function (event) {
+             if (checkMail(this.value)) {
+                 this.setCustomValidity("");
+                 this.style.outline = "1px solid green";
+             }else {
+                 this.setCustomValidity("Erreur: l'adresse mail n'est pas au bon format : exemple@gmail.com");
+                 this.style.outline = "1px solid red";
+             }
+         })
+     }
+}
+
+function checkPassword(password) {
+    return password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/);
+}
+
+function passwordValidity (input) {
     input.addEventListener("input", function (event) {
-        if (checkMail(this.value)) {
+        if (checkPassword(this.value)) {
             this.setCustomValidity("");
             this.style.outline = "1px solid green";
         }else {
-            this.setCustomValidity("Erreur: l'adresse mail n'est pas au bon format : exemple@gmail.com");
+            this.setCustomValidity("Erreur: Le mot de passe doit contenir au moins 1 majuscule, une minuscule et un chiffre/nombre");
             this.style.outline = "1px solid red";
         }
     })
 }
+
 
 if (username) {
     checkRange(4, 40, username, "La longueur du pseudo doit-être comprise entre 4 et 40 caractères.");
@@ -44,6 +63,7 @@ if (username) {
 
 if(password) {
     checkRange(8, 25, password, "La longueur du password doit-être comprise entre 8 et 25 caractères.");
+    passwordValidity(password);
 }
 
 if (subject) {
@@ -54,10 +74,9 @@ if (contact) {
     checkRange(4, 60, contact, "La longueur de votre message doit-être comprise entre 20 et 255 caractères.");
 }
 if (email) {
+    checkRange(6 , 150, email, 'La longueur du mail doit-être comprise entre 6 et 150 caractères');
     mailValidity(email);
-    checkRange(email, 6, 150, 'La longueur du mail doit-être comprise entre 6 et 150 caractères');
 }
-
 
 
 if (errorOrSuccessMessage) {
