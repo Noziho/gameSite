a5087d4d656b7fafa3910fe8aaea8083dc72e0aa
noahd <noah.decroix3@gmail.com>
2022-05-15 12:17:12 +0200

    check mail format + fix check range

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,18 +5,37 @@ const subject = document.querySelector('#subject');
 const contact = document.querySelector('#message');
 const menuLogo = document.querySelector(".fa-bars");
 const menu = $(".header_menu");
+const email = document.querySelector('#email');
 
 
  function checkRange (min, max , input, errorMessage) {
-    input.addEventListener('keypress', function () {
-        if (input.value.length < min || input.value.length > max) {
-            input.setCustomValidity(errorMessage);
-        }
+    input.addEventListener('input', function (event) {
+        if (this.value.length > min && this.value.length < max) {
+            this.setCustomValidity("");
+            this.style.outline = "1px solid green"
+;        }
         else {
-            input.setCustomValidity("");
+            this.setCustomValidity(errorMessage);
+            this.style.outline = "1px solid red";
         }
     })
 
+}
+
+function checkMail (mail) {
+    return mail.match(/[a-z0-9]+@[a-z]+.[a-z]{2,3}/)
+}
+
+function mailValidity (input) {
+    input.addEventListener("input", function (event) {
+        if (checkMail(this.value)) {
+            this.setCustomValidity("");
+            this.style.outline = "1px solid green";
+        }else {
+            this.setCustomValidity("Erreur: l'adresse mail n'est pas au bon format : exemple@gmail.com");
+            this.style.outline = "1px solid red";
+        }
+    })
 }
 
 if (username) {
@@ -34,6 +53,10 @@ if (subject) {
 if (contact) {
     checkRange(4, 60, contact, "La longueur de votre message doit-être comprise entre 20 et 255 caractères.");
 }
+if (email) {
+    mailValidity(email);
+    checkRange(email, 6, 150, 'La longueur du mail doit-être comprise entre 6 et 150 caractères');
+}
 
 
 
@@ -50,3 +73,7 @@ if (errorOrSuccessMessage) {
 menuLogo.addEventListener("click", () => {
     menu.toggleClass('visible')
 });
+
+
+
+
