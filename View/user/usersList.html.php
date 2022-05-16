ee077bc77898421f93b4e46323899789ac5d38c0
noahd <noah.decroix3@gmail.com>
2022-05-16 19:21:46 +0200

    remove useless code on usersList page

--- View/user/usersList.html.php
+++ View/user/usersList.html.php
@@ -37,56 +37,50 @@ if (isset($data['users'])) {
 <div class="container">
     <div id="usersListContainer"><?php
         foreach ($users as $user) {
-            /* @var User $user */ ?>
-            <div class="usersList">
+        /* @var User $user */ ?>
+        <div class="usersList">
             <p>Mail: <?= $user->getEmail() ?></p>
             <p>Pseudo: <?= $user->getUsername() ?></p>
             <?php
             foreach ($user->getRole() as $role) {
                 /* @var Role $role */ ?>
                 <p>Rôle actuel: <?= $role->getName(); ?></p><?php
-            }
 
-            if (AbstractController::isAdmin()) { ?>
-                <p>Id: <?= $user->getId() ?></p>
-                <form id="edit_role" action="/?c=user&a=edit-user-role&id=<?= $user->getId() ?>" method="post">
-                        <label for="role">Mofidiez le rôle:</label>
-                        <select name="role" id="role">
-                            <option value="1">Utilisateur</option>
-                            <option value="2">Modérateur</option>
-                        </select>
-
-                        <input id="editUser" type="submit" name="submit" value="Modifiez">
-                    </form>
-
-                    <div id="delete_edit_button">
-                    <a id="deleteUser" href="/?c=user&a=delete-user&id=<?= $user->getId() ?>">Supprimez
-                        l'utilisateur</a><br>
+                if ($role->getName() === 'mute') { ?>
+                    <div>
+                    <a href="/?c=user&a=unmute&id=<?= $user->getId() ?>">Démute l'utilisateur</a>
+                    </div><?php
+                }
+                elseif ($role->getName() === 'user') { ?>
+                    <div>
+                        <a href="/?c=user&a=mute&id=<?= $user->getId() ?>">Muté l'utilisateur</a>
                     </div>
-                <?php
+
+                    <div>
+                    <a href="/?c=user&a=last-messages&id=<?= $user->getId() ?>">Voir les 100 derniers messages</a>
+                    </div><?php
+                }
+
             }
+            if (AbstractController::isAdmin()) { ?>
+            <p>Id: <?= $user->getId() ?></p>
+            <form id="edit_role" action="/?c=user&a=edit-user-role&id=<?= $user->getId() ?>" method="post">
+                <label for="role">Mofidiez le rôle:</label>
+                <select name="role" id="role">
+                    <option value="1">Utilisateur</option>
+                    <option value="2">Modérateur</option>
+                </select>
 
-            if (AbstractController::isAdmin() || AbstractController::isModerator()) { ?>
-                    <?php
-                    foreach ($user->getRole() as $role) {
-                        /* @var Role $role */
-                        if ($role->getName() === 'mute') {?>
-                            <div>
-                                <a href="/?c=user&a=unmute&id=<?= $user->getId() ?>">Demute l'utilisateur</a>
-                            </div><?php
-                        }
-                        elseif ($role->getName() === 'user') {?>
-                            <div>
-                                <a href="/?c=user&a=mute&id=<?= $user->getId() ?>">Muté l'utilisateur</a>
-                            </div>
+                <input id="editUser" type="submit" name="submit" value="Modifiez">
+            </form>
 
-                            <div>
-                                <a href="/?c=user&a=last-messages&id=<?= $user->getId() ?>">Voir les 100 derniers messages</a>
-                            </div><?php
-                        }
-                    }
+            <div id="delete_edit_button">
+                <a id="deleteUser" href="/?c=user&a=delete-user&id=<?= $user->getId() ?>">Supprimez
+                    l'utilisateur</a><br>
+            </div>
+            <?php
             } ?>
-            </div><?php
+        </div><?php
 
         } ?>
     </div>
